596f913eee832bab845dd766a00fd5f8052ccaaa
solenemhep <solene.pettier@gmail.com>
2021-07-12 17:57:20 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require("http-errors")
+var express = require("express")
+var path = require("path")
+var cookieParser = require("cookie-parser")
+var logger = require("morgan")
+var fs = require("fs")
+
+var indexRouter = require("./routes/index")
+var usersRouter = require("./routes/users")
+
+var app = express()
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "jade")
+
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use("/", indexRouter)
+app.use("/users", usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render("error")
+})
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+})
+
+// setup the logger
+app.use(logger("combined", { stream: accessLogStream }))
+
+app.get("/", function (req, res) {
+  res.send("hello, world!")
+})
+
+module.exports = app
